4c6e0a0730e4fa562dc6d3c7009373d292d2004b
Evgeniy Malygin <black2001@inbox.ru>
2022-08-29 12:42:48 +0300

    Выполнено задание 3

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,14 @@ public class Main {
         // Задание 2
         System.out.println("========== Задание 2 ==========");
         System.out.printf("ФИО сотрудника - %s \n", fullName.toUpperCase());
+
+        // Задание 3
+        System.out.println("========== Задание 3 ==========");
+        firstName = "Семён";
+        middleName = "Семёнович";
+        lastName = "Иванов";
+        fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.printf("ФИО сотрудника - %s \n", fullName);
     }
 }
